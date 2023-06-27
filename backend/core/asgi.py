8e039aa5338689988129dd4d4aee1444079f50c5
gonin <goninha@outlook.fr>
2023-06-27 16:52:08 +0100

    push

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -12,7 +12,6 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
-from core.routing import websocket_urlpatterns
 import os
 
 from django.core.wsgi import get_wsgi_application
--- backend/core/settings/dev.py
+++ backend/core/settings/dev.py
@@ -1,7 +1,5 @@
 from .base import *
 
-import logging
-
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
