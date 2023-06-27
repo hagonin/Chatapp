73c482e2c5edd171414ac1ab6c0f2ed625568728
gonin <goninha@outlook.fr>
2023-06-27 16:21:15 +0100

    Delete settings in urls.py

--- backend/core/routing.py
+++ backend/core/routing.py
@@ -1,7 +1,7 @@
-from django.urls import re_path
+# from django.urls import re_path
 
-from . import consumers
+# from . import consumers
 
-websocket_urlpatterns = [
-    re_path(r'ws/chat/$', consumers.ChatConsumer.as_asgi()),
-]
+# websocket_urlpatterns = [
+#     re_path(r'ws/chat/$', consumers.ChatConsumer.as_asgi()),
+# ]
