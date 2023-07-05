dc6b8f4fab385b322092599e37147380b6429530
gonin <goninha@outlook.fr>
2023-07-05 18:14:48 +0100

    Fix - url server-side

--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -3,15 +3,15 @@ from .base import *
 DEBUG = False
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-ALLOWED_HOSTS = ['talkie-api.up.railway.app', 'localhost',
+ALLOWED_HOSTS = ['talkie-api.vercel.app', 'localhost',
                 '192.168.1.87',]
 CSRF_TRUSTED_ORIGINS = [
-    'https://talkie-api.up.railway.app',
+    'https://talkie-api.vercel.app',
     'http://localhost:3000',
     'http://192.168.1.87:3000',
 ]
 CORS_ALLOWED_ORIGINS = [
-    "https://talkie-api.up.railway.app",
+    "https://talkie-api.vercel.app",
     'http://localhost:3000',
     'http://192.168.1.87:3000',
 ]
